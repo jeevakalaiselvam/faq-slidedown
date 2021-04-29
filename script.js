920ee338711fe7fdd71fbda7e25062a16d30f256
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-30 04:48:19 +0530

    screens added

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.getElementById("container");
 
 const questions = [
-    "Question1",
-    "Question2",
-    "Question3",
-    "Question4",
-    "Question5",
+    "How do the different plans compare?",
+    "I already have an Evernote Basic account. How do I select a different plan?",
+    "I've upgraded but nothing has changed. What should I do?",
+    "Can I use Evernote Points, redemption codes, or gift cards to pay for a subscription?",
+    "Can I subscribe to Evernote Plus?",
 ];
 
-const answers = ["Answer1", "Answer2", "Answer3", "Answer4", "Answer5"];
+const answers = [
+    `For a detailed comparison of Evernote account plans, see this <a href="https://help.evernote.com/hc/en-us/articles/209005157">article.<a/>`,
+    `Go to your <a href="https://www.evernote.com/Settings.action">account settings</a> to upgrade.`,
+    `Make sure you <a href="https://help.evernote.com/hc/articles/209005257">have the latest version of Evernote</a> installed on all your devices and sync your account to take advantage of all the new features.`,
+    `Yes. <a href="https://help.evernote.com/hc/articles/208314268">Evernote Points</a>, valid redemption codes, or gift cards may be used towards Evernote Premium.`,
+    `As of early April 2018, Evernote Plus is no longer available for purchase. To get more features than Basic provides, upgrade to Premium. Note: Current Plus subscribers will maintain their subscription as long as their subscription remains active.`,
+];
 
 function setupFAQ() {
     questions.forEach((question, index) => {
