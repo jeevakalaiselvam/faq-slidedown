66db920b62b1944b0fc2de4257b3c7de3c132ed6
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-30 05:30:12 +0530

    docs added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Get reference for root container for manipulation later
 const container = document.getElementById("container");
 
+//Declare all questions for FAQ
 const questions = [
     "How do the different plans compare?",
     "I already have an Evernote Basic account. How do I select a different plan?",
@@ -8,6 +10,7 @@ const questions = [
     "Can I subscribe to Evernote Plus?",
 ];
 
+//Declare all answers for FAQ
 const answers = [
     `For a detailed comparison of Evernote account plans, see this <a href="https://help.evernote.com/hc/en-us/articles/209005157">article.<a/>`,
     `Go to your <a href="https://www.evernote.com/Settings.action">account settings</a> to upgrade.`,
@@ -16,10 +19,14 @@ const answers = [
     `As of early April 2018, Evernote Plus is no longer available for purchase. To get more features than Basic provides, upgrade to Premium. Note: Current Plus subscribers will maintain their subscription as long as their subscription remains active.`,
 ];
 
+/**
+ * This function loops through all questions and answers and generates a new FAQ card and appends it to the root container
+ */
 function setupFAQ() {
     questions.forEach((question, index) => {
         const card = document.createElement("div");
 
+        //Generation of FAQ cards
         card.innerHTML = `
             <div class="faq-card" id="faq-card${index}">
                 <h2 class="question">${questions[index]}</h2>
@@ -29,19 +36,24 @@ function setupFAQ() {
             </div>
         `;
 
+        //Append to root container
         container.appendChild(card);
 
         const cardMain = document.getElementById(`faq-card${index}`);
         const openBtn = document.getElementById(`open-${index}`);
         const closeBtn = document.getElementById(`close-${index}`);
+
+        //When chevron icon is clicked, The answer element is shown below.
         openBtn.addEventListener("click", () => {
             cardMain.classList.add("active");
         });
 
+        //When close icon is clicked, The answer element is hidden
         closeBtn.addEventListener("click", () => {
             cardMain.classList.remove("active");
         });
     });
 }
 
+//Initiate FAQ setup
 setupFAQ();
